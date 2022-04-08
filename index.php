5de20beeea3c601f2847f9b2157e83077a24466b
Shivankit <shivankitraghav396@gmail.com>
2022-04-08 16:53:27 +0530

    Made the list

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+    <title>To-do list</title>
+</head>
+<body>
+    <div class="main">
+        <div class="add">
+            <form action="">
+                <input type="text" name="title" placeholder="Enter the task">
+                <button type="submit">Add</button>
+            </form>
+        </div>
+        <div class="tasks">
+            <div class="item">
+                <input type="checkbox">
+                <h2>This is trial task</h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
